8e595e68415b38e5238e4f24ea8786c48ceb705f
nisha74-hub <nishaptl105@gmail.com>
2025-06-10 23:30:10 +0530

    Update real-estate-tokenization-platform.sol

--- contracts/real-estate-tokenization-platform.sol
+++ contracts/real-estate-tokenization-platform.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 contract RealEstateTokenization is ERC721, Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter privat
